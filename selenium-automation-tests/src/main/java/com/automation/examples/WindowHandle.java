605b40d9925caaa784fffae613308e109d0f6e67
priyaranjantah <priyaranjantah@gmail.com>
2017-06-12 16:58:24 +0530

    commit branch

--- selenium-automation-tests/src/main/java/com/automation/examples/WindowHandle.java
+++ selenium-automation-tests/src/main/java/com/automation/examples/WindowHandle.java
@@ -12,7 +12,7 @@ public class WindowHandle {
 
     public static void main(String[] args) throws InterruptedException {									
         	
-    	System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
+    	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
     	
 		driver.get("http://www.yatra.com/");
